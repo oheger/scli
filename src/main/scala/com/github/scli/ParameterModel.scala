1e3ebb601c253a134f93bfd5780b5e8016dcdb0a
oheger <oliver.heger@oliver-heger.de>
2021-10-10 19:25:14 +0200

    Updated copyright dates to include 2021.

--- src/main/scala/com/github/scli/ParameterModel.scala
+++ src/main/scala/com/github/scli/ParameterModel.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Developers Team.
+ * Copyright 2020-2021 The Developers Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
